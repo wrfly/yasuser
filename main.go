afb7046c941ff16db27008d20cae129ec19c988b
wrfly <mr.wrfly@gmail.com>
2018-04-20 23:32:23 +0800

    refactor: routes

--- main.go
+++ main.go
@@ -1,47 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"os"
 	"strings"
 
 	"github.com/sirupsen/logrus"
 
-	"github.com/gin-gonic/gin"
 	"github.com/wrfly/short-url/config"
 	"github.com/wrfly/short-url/handler"
 	"github.com/wrfly/short-url/handler/db"
+	"github.com/wrfly/short-url/routes"
 	"gopkg.in/urfave/cli.v2"
 )
-
-func srv(conf *config.Config, shorter *handler.Shorter) error {
-	example := "curl https://u.kfd.me -d \"http://longlonglong.com/long/long/long?a=1&b=2\""
-	srv := gin.Default()
-	srv.GET("/:url", func(c *gin.Context) {
-		location := shorter.Long(c.Param("url"))
-		if location == "" {
-			c.String(404, example)
-			return
-		}
-		c.Redirect(302, location)
-	})
-	srv.GET("/", func(c *gin.Context) {
-		c.String(200, example)
-	})
-	srv.POST("/", func(c *gin.Context) {
-		b, err := ioutil.ReadAll(c.Request.Body)
-		if err != nil {
-			logrus.Error(err)
-			c.String(500, err.Error())
-		}
-		short := shorter.Short(string(b))
-		shortURL := fmt.Sprintf("%s/%s\n", conf.Prefix, short)
-		c.String(200, shortURL)
-	})
-	port := fmt.Sprintf(":%d", conf.Port)
-	return srv.Run(port)
-}
 
 func main() {
 	conf := config.Config{}
@@ -117,7 +87,7 @@ func main() {
 			shorter := handler.Shorter{
 				DB: shorterDB,
 			}
-			return srv(&conf, &shorter)
+			return routes.Serve(&conf, &shorter)
 		},
 	}
 
---
+++ routes/serve.go
@@ -0,0 +1,39 @@
+package routes
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"github.com/wrfly/short-url/config"
+	"github.com/wrfly/short-url/handler"
+)
+
+func Serve(conf *config.Config, shorter *handler.Shorter) error {
+	example := "curl https://u.kfd.me -d \"http://longlonglong.com/long/long/long?a=1&b=2\""
+	srv := gin.Default()
+	srv.GET("/:url", func(c *gin.Context) {
+		location := shorter.Long(c.Param("url"))
+		if location == "" {
+			c.String(404, example)
+			return
+		}
+		c.Redirect(302, location)
+	})
+	srv.GET("/", func(c *gin.Context) {
+		c.String(200, example)
+	})
+	srv.POST("/", func(c *gin.Context) {
+		b, err := ioutil.ReadAll(c.Request.Body)
+		if err != nil {
+			logrus.Error(err)
+			c.String(500, err.Error())
+		}
+		short := shorter.Short(string(b))
+		shortURL := fmt.Sprintf("%s/%s\n", conf.Prefix, short)
+		c.String(200, shortURL)
+	})
+	port := fmt.Sprintf(":%d", conf.Port)
+	return srv.Run(port)
+}
